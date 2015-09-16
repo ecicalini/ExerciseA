9eec82a0b6010958c9fcc2090153540861c058e5
Elena Cicalini <elena.cicalini@gmail.com>
2015-09-16 15:04:02 +0100

    tolto commento

--- app/controllers/cardStatusCtrl.js
+++ app/controllers/cardStatusCtrl.js
@@ -1,9 +1,4 @@
 app.controller("cardStatusCtrl", function($scope) {
-//	$scope.message = "";
-//	$scope.left  = function() {return 100 - $scope.message.length;};
-//	$scope.clear = function() {$scope.message = "";};
-//	$scope.save  = function() {alert("Note Saved");};
-
 	$scope.cardStatus = cardStatus;
 });
 
