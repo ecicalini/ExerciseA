ed014ea244210ba4d340aad0d3db08a265365528
Elena Cicalini <elena.cicalini@gmail.com>
2015-09-18 00:08:09 +0200

    random color on the cards

--- app/controllers/cardStatusCtrl.js
+++ app/controllers/cardStatusCtrl.js
@@ -1,6 +1,27 @@
 app.controller("cardStatusCtrl", function($scope) {
 	$scope.cardStatus = cardStatus;
+	
+	$scope.color = function (card) {
+	    if (card.color) {
+	        return card.color;
+	    }
+
+	    var letters = '012345'.split('');
+	    var color = '#';
+	    color += letters[Math.round(Math.random() * 5)];
+	    letters = '0123456789ABCDEF'.split('');
+	    for (var i = 0; i < 5; i++) {
+	        color += letters[Math.round(Math.random() * 15)];
+	    }
+
+	    // assign the color so it doesn't freak out
+	    card.color = color;
+
+	    return color;
+	};
 });
+
+
 
 var cardStatus =[{
 					"type": "b&g",
